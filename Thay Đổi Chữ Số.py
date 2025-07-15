72802bbff44d71f8e0674b5e9fb1d4dce783fa5f
Cherry3224 <148939269+Cherry3224@users.noreply.github.com>
2025-07-15 13:52:17 +0700

    Thay Đổi Chữ Số.py

--- Thay Đổi Chữ Số.py
+++ Thay Đổi Chữ Số.py
@@ -1,21 +1,21 @@
-def to(a, b, s: str): # s : str (Ép kiểu string cho s)
-    return int(s.replace(str(a), str(b)))
+def Convert(q, p, s : str): # s : str (Ép kiểu string cho s)
+    return int(s.replace(str(q), str(p)))
 
 for _ in range(int(input())):
-    a, b = input().split()
+    q, p = input().split()
 
-    while True:
+    while True: # Xử lý input ko sạch
         line = input().strip()
 
         if line:
             break
+
     if len(line.split()) == 2:
-        x1, x2 = line.split()
+        X1, X2 = line.split()
 
     else:
-        x1 = line
+        X1 = line
 
-        x2 = input().strip()
+        X2 = input().strip()
 
-    print(to(b, a, x1) + to(b, a, x2), to(a, b, x1) + to(a, b, x2))
-
+    print(Convert(p, q, X1) + Convert(p, q, X2), Convert(q, p, X1) + Convert(q, p, X2))
